52a83a83c3fc217cd6d8c3cee5e0bdd44554e21a
Hayley K <mrs.kornilenko@gmail.com>
2019-06-18 21:26:57 -0400

    setup back end express server

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// An api endpoint that returns the mealplan of items
+app.get('/api/mealplan', (req,res) => {
+    var mealplan = ["wednesday", "sunday", "monday"];
+    res.json(mealplan);
+    console.log('Sent mealplan items');
+});
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log('App is listening on port ' + port);
